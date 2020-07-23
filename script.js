67c42859c8025dd9fe990310f3e43059beebe937
Elham Fabe <elhamfabe@gmail.com>
2020-07-22 21:19:48 -0400

    created prompted user questions and generated password

---
+++ script.js
@@ -0,0 +1,35 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var passwordLength = prompt("How long do you want your password to be (8-128 characters)")
+
+var passwordLower = confirm("Do you want lowercase letters?")
+
+var passwordUpper = confirm("Do you want upper letters?")
+
+var passwordNumeric = confirm("Do you want numbers?")
+
+var passwordSpecial = confirm("Do you want any special characters?")
+
+function generatePassword() {
+  var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var lower = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  var upper = 'abcdefghijklmnopqrstuvwxyz'.toUppercase().split('')
+  var special = '!@#%^&**()_'.split('')
+  var chosenCharacters = []
+}
+
+
+
